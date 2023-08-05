36fcc3fde12b4f860a36dd08ae3c96541a619a70
Do Minh Quan <dominhquan9094@gmail.com>
2023-08-05 05:56:49 +0000

    20230805_Upload script

---
+++ ohmyzsh_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install zsh -y
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh --version
+
+
+omz update
---
+++ ping_install.sh
@@ -0,0 +1,5 @@
+apt-get update -y
+apt-get install -y iputils-ping
+
+sudo apt-get update -y
+sudo apt-get install -y build-essential
